e93e3df35d2987ab7a1361e73d7e07517da432ee
Jean <jxc3889@rit.edu>
2014-10-29 13:46:15 -0400

    Added List<String> before end of class

--- CGrep.java
+++ CGrep.java
@@ -1,7 +1,10 @@
-// CGrep handles the user input and provides it to the Finder
-public class CGrep {
+package java.util.regex;
+import java.awt.List;
+import java.util.ArrayList;
 
+
+public class CGrep {
 	public static void main( String[] args ) {
-	
-	}
+		List<String> entry = new ArrayList<String>() ;
+	}  
 }
