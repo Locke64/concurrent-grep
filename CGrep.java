6e3583c38dc272de16a4ff71f1fd9498c192ede3
Jean Cheng <jxc3889@rit.edu>
2014-10-30 18:11:16 -0400

    Started CGrep

--- CGrep.java
+++ CGrep.java
@@ -1,10 +1,23 @@
-package java.util.regex;
+
 import java.awt.List;
 import java.util.ArrayList;
 
 
-public class CGrep {
-	public static void main( String[] args ) {
-		List<String> entry = new ArrayList<String>() ;
+public class CGrep 
+{
+	public static void main( String[] args ) 
+	{
+		String pattern = null;
+				
+		if (pattern != args[0])
+		{
+			System.out.println("Please input the correct format.");
+		}
+		else
+		{
+		//TODO
+		//List<String> entry = new ArrayList<String>() ;
+		}
+		
 	}  
 }
